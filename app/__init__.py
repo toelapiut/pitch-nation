7c5edc19e7929e1973d2cfe3e3d62ca40748edc4
toelapiut <toelapiut7@gmail.com>
2017-10-28 09:39:29 +0300

    created a blue print and initialized it

--- app/__init__.py
+++ app/__init__.py
@@ -15,4 +15,9 @@ def create_app(config_name):
     # Initializing flask extensions
     bootstrap.init_app(app)
 
+
+    #Register the blueprint 
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
     return app
