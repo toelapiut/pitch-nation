73aba2b1f03461acd9cba014615a1a3a5a014f13
toelapiut <toelapiut7@gmail.com>
2017-10-29 17:04:40 +0300

    added custom validators

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,7 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,PasswordField,SubmitField
+from wtforms import StringField,PasswordField,SubmitField,ValidationError
 from wtforms.validators import Required,Email,EqualTo
 from ..models import User
+
 
 
 class RegistrationForm(FlaskForm):
@@ -10,3 +11,11 @@ class RegistrationForm(FlaskForm):
      password=PasswordField('Password',validators=[Required(),EqualTo('password_confirm',message='passwords must match')])
      password_confirm=PasswordField('Confirm Password',validators=[Required()])
      submit=SubmitField('Sign Up')
+
+     def validator_email(self,data_field):
+          if User.query.filter_by(email=data_field.data).first():
+               raise ValidationError('There is an account with that email')
+
+     def validator_username(self,data_field):
+          if User.query.filter_by(username=data_field.data).first():
+               raise ValidationError('The username is taken')
