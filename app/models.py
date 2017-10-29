5b1a6f3fdcbdd6c593be57b865ee4748ec57051e
toelapiut <toelapiut7@gmail.com>
2017-10-29 16:22:20 +0300

    Added mixin and login manager to get user id

--- app/models.py
+++ app/models.py
@@ -1,12 +1,15 @@
 from . import db
 from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin
+from . import login_manager
 
 ##In here we will have our Class that contain instances
 
-class User(db.Model):
+class User(UserMixin,db.Model):
   __tablename__='users'
   id=db.Column(db.Integer,primary_key=True)
   username=db.Column(db.String(255))
+  email=db.Column(db.String(255),unique=True,index=True)
   role_id=db.Column(db.Integer,db.ForeignKey('roles.id'))
   pass_secure=db.Column(db.String(Length(min=7,max=80)))
 
@@ -20,7 +23,10 @@ class User(db.Model):
 
     def verify_password(self,password):
       return check_password_hash(self.pass_secure,password)
-
+@login_manager.user_loader
+def loader_user(user_id):
+	return User.query.get(int(user_id))
+    
     
   def __repr__(self):
     return f'User {self.username}'
