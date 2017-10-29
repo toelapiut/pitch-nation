b198d2cfe9e935ee3c4a48d8360c82a0005acf8a
toelapiut <toelapiut7@gmail.com>
2017-10-29 12:02:32 +0300

    added a user column

--- app/models.py
+++ app/models.py
@@ -1,1 +1,10 @@
+from . import db
 ##Here we will have our Class that contain instances
+
+class User(db.Model):
+  __tablename__='users'
+  id=db.Column(db.Integer,primary_key=True)
+  username=db.Column(db.String(255))
+
+  def __repr__(self):
+    return f'User {self.username}'
