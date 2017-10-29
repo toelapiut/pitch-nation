f5d6e026d7d6a0934da27a018a9f1c6d89bf70cc
toelapiut <toelapiut7@gmail.com>
2017-10-29 17:35:50 +0300

    added a login and log out route

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,12 +1,24 @@
 from flask import render_template,redirect,url_for
 from ..models import User
-from .forms import RegistrationForm
+from .forms import RegistrationForm,LoginForm
 from .. import db
 from . import auth 
+from flask_login import login_user,logout_user,login_required
 
-@auth.route('./login')
+@auth.route('./login',methods=['GET','POST'])
 def login():
-     return render_template('auth/login.html')
+     login_form=LoginForm()
+     if login_form.validate_on_submit():
+          user=User.query.filter_by(email=Login_form.email.data).first()
+          if user is not None and user.verify_password(login_form.password.data):
+               login_user(user,login_form.remember.data)
+               return redirect(request.args.get('next') or url_for('main.index'))
+
+          flash('Invalid username or password')
+
+     title="welcome Back "
+
+     return render_template('auth/login.html' ,login_form=login_form,title=title)
 
 @auth.route('/register',methods=['GET','POST'])
 def register():
@@ -18,3 +30,9 @@ def register():
           return redirect(url_for('auth.login'))
           title="New account"
      return render_template('auth/register.html',registration_form = form)
+
+@auth.route('/logout')
+@login_required
+def logout():
+     logout_user()
+     return redirect(url_for('main.index'))
