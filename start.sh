56f87d49f2c114f8c22e8a52637c840b3afbce6f
toelapiut <toelapiut7@gmail.com>
2017-10-29 22:28:05 +0300

    added email

--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
 export API_KEY=<Your api key>
 export SECRET_KEY=<Your secret key>
+export MAIL_USERNAME=<Your Email Address>
+export MAIL_PASSWORD=<Your Email Password>
 
 python3.6 manage.py server
