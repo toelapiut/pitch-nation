89958306d8fa6dcbd5f7b9171348c05103cb11fb
toelapiut <toelapiut7@gmail.com>
2017-10-29 15:14:42 +0300

    added test to raise attribute and verify

--- tests/test_user.py
+++ tests/test_user.py
@@ -2,8 +2,17 @@ import unittest
 from app,models import User
 
 class UserModelTest(unittest.TestCase):
-  def setUp(self):
-    self.new_user=User(password='bananass')
+      def setUp(self):
+            self.new_user=User(password='bananass')
 
-  def test_password_setter(self):
-    self.assertTrue(self.new_user.pass_secure is not True)
+      def test_password_setter(self):
+            self.assertTrue(self.new_user.pass_secure is not True)
+
+
+      def test_no_access_password(self):
+            with self.assertRaises(AttributeError):
+                self.new_user.password
+
+      def test_password_verification(self):
+            self.assertTrue(self.new_user.verify_password('banana'))
+
