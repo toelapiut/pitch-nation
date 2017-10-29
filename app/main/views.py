699554df9cb85d74bc967565cde4d27340ddc582
toelapiut <toelapiut7@gmail.com>
2017-10-29 20:32:09 +0300

    added routes to profile..

--- app/main/views.py
+++ app/main/views.py
@@ -1,8 +1,8 @@
-from flask import render_template,request,redirect,url_for
+from flask import render_template,request,redirect,url_for,abort
 from . import main
 ##remember to import classes from ..requests
-from .forms import ReviewForm
-
+from .forms import ReviewForm,UpdateProfile
+from .. import db,photos
 '''
 We then define our route decorators using the 
 main blueprint instance instead of the app instance
@@ -17,4 +17,43 @@ def index():
     message='Hello World Welcome to flask'
 
     return render_template('index.html', message=message)
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user=User.query.filter_by(username=uname).first()
+    
+    if user is None:
+        abort(404)
+
+    return render_template('profile/profile.html',user=user)
+
+
+@main.route('/user/<uname>/update',methods=['GET','POST'])
+@login_required
+def update_profile(uname):
+    user=User.query.filter_by(username=uname).first()
+    if user is None:
+        abort(404)
+    form=UpdateProfile()
+
+    if form.validate_on_submit():
+        user.bio=form.bio.data
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile',uname=user.username))
+
+    return render_template('profile/update.html',form=form)
+
+
+@main.route('/user/<uname>/update/pic',methods=['POST'])
+@login_required
+def  update_pic(uname):
+    user=User.query.filter_by(username=uname).first()
+    if 'photos' in request.files:
+        filename=photos.save(request.files['photo'])
+        path=f'photos/{filename}'
+        user.profile_pic_path=path
+        db.session.commit()
+    return redirect(url_for('main.profile',uname=uname))
 
