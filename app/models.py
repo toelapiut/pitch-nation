87f5fc85001ef065c8aea10903ac33b602a77d06
toelapiut <toelapiut7@gmail.com>
2017-10-29 14:41:16 +0300

    added properties to secure passwords

--- app/models.py
+++ app/models.py
@@ -10,6 +10,18 @@ class User(db.Model):
   role_id=db.Column(db.Integer,db.ForeignKey('roles.id'))
   pass_secure=db.Column(db.String(Length(min=7,max=80)))
 
+    @property
+    def password(self):
+      raise AttributeError('You cannot read the attribute')
+
+    @password.setter
+    def password(self,password):
+      self.pass_secure=generate_password_hash(password)
+
+    def verify_password(self,password):
+      return check_password_hash(self.pass_secure,password)
+
+    
   def __repr__(self):
     return f'User {self.username}'
 
