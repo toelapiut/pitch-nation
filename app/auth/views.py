14055dff1921454f73a04e748c43fc7337a4289c
toelapiut <toelapiut7@gmail.com>
2017-10-29 22:27:47 +0300

    added email

--- app/auth/views.py
+++ app/auth/views.py
@@ -4,6 +4,7 @@ from .forms import RegistrationForm,LoginForm
 from .. import db
 from . import auth 
 from flask_login import login_user,logout_user,login_required
+from ..email import mail_message
 
 @auth.route('/login',methods=['GET','POST'])
 def login():
@@ -27,6 +28,8 @@ def register():
           user=User(email=form.email.data,username=form.username.data,password=form.password.data)
           db.session.add(user)
           db.session.commit()
+
+          mail_message('welcome to ..........." email/welcome_user',user.email,user=user)
           return redirect(url_for('auth.login'))
           title="New account"
      return render_template('auth/register.html',registration_form = form)
