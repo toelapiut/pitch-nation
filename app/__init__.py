76bf1175e89f9e4ae11af72750adff2f0ba2d1e3
toelapiut <toelapiut7@gmail.com>
2017-10-29 15:47:01 +0300

    Initialized authentication in __init__ inside app

--- app/__init__.py
+++ app/__init__.py
@@ -2,15 +2,26 @@ from flask import Flask
 from flask_bootstrap import Bootstrap
 from config import config_options 
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 
 bootstrap=Bootstrap(app)
 db=SQLAlchemy()
 
+
 def create_app(config_name):
+
+    #initializing login
+    login_manager = LoginManager()
+    login_manager.session_protection = 'strong'
+    login_manager.login_view = 'auth.login'
 
     # Initializing application
     app = Flask(__name__)
+
+    #registration of the blueprint
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint,url_prefix='/authenticate')
 
     # Creating the app configurations
     app.config.from_object(config_options[config_name])
