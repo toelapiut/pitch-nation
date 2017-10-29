cce49714e9b58fca4c26964c755921b892aa5e64
toelapiut <toelapiut7@gmail.com>
2017-10-29 13:06:59 +0300

    Added a config to help run tests in the terminal

--- manage.py
+++ manage.py
@@ -1,6 +1,7 @@
 from app import create_app
 from flask_script import Manager,Server
-
+from app import create_app,db
+from app.models import User
 # Creating app instance
 app = create_app('development')
 
@@ -14,6 +15,9 @@ def test():
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
 
+@manager.shell
+def make_shell_context():
+    return dict(app=app,db=db,User=User)
 
 
 if __name__=='__main__':
