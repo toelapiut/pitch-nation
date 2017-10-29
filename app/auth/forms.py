ebde22d0144afc61fe919e12871c4eeeb3e9c2ee
toelapiut <toelapiut7@gmail.com>
2017-10-29 17:04:59 +0300

    fix bug

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,PasswordField,SubmitField,ValidationError
+from wtforms import StringField,PasswordField,SubmitField,ValidationError,BooleanField
 from wtforms.validators import Required,Email,EqualTo
 from ..models import User
 
@@ -19,3 +19,5 @@ class RegistrationForm(FlaskForm):
      def validator_username(self,data_field):
           if User.query.filter_by(username=data_field.data).first():
                raise ValidationError('The username is taken')
+
+
