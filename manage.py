9d9ca1f0f67e033d46c48328fa992d171611a68d
toelapiut <toelapiut7@gmail.com>
2017-10-29 14:20:38 +0300

    added migrate

--- manage.py
+++ manage.py
@@ -2,11 +2,17 @@ from app import create_app
 from flask_script import Manager,Server
 from app import create_app,db
 from app.models import User
+from flask_migrate import Migrate,MigrateCommand
+
 # Creating app instance
 app = create_app('development')
 
 manager = Manager(app)
 manager.add_command('server',Server)
+
+#initialize the Migrate 
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
 
 @manager.add_command
 def test():
@@ -17,7 +23,7 @@ def test():
 
 @manager.shell
 def make_shell_context():
-    return dict(app=app,db=db,User=User)
+    return dict(app=app,db=db,User=User,Role=Role)
 
 
 if __name__=='__main__':
