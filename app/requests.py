819c5c6322740f59f019b3bdb2ed01e974035ae3
toelapiut <toelapiut7@gmail.com>
2017-10-30 21:19:30 +0300

    commented out code for test

--- app/requests.py
+++ app/requests.py
@@ -1,17 +1,17 @@
-import urllib.request,json
-##import a class from models eg from .models import Movie
+# import urllib.request,json
+# ##import a class from models eg from .models import Movie
 
-# Getting api key
-api_key = None
+# # Getting api key
+# api_key = None
 
-# Getting the  base url
-base_url = None
+# # Getting the  base url
+# base_url = None
 
-def configure_request(app):
-    global api_key,base_url
-    '''
-    Here we can import  below and below is just but an example 
-    if u dont need them you can remove them
-    '''
-    api_key = app.config['API_KEY']
-    base_url = app.config['BASE_URL']
+# def configure_request(app):
+#     global api_key,base_url
+#     '''
+#     Here we can import  below and below is just but an example 
+#     if u dont need them you can remove them
+#     '''
+#     api_key = app.config['API_KEY']
+#     base_url = app.config['BASE_URL']
