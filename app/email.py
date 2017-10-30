89c68a464faa0e5b0a96925c7b23c104dac3b8cf
toelapiut <toelapiut7@gmail.com>
2017-10-30 21:18:38 +0300

    key my password

--- app/email.py
+++ app/email.py
@@ -3,7 +3,7 @@ from flask import render_template
 from . import mail
 
 def mail_message(subject,template,to,**kwargs):
-     send_email=<your Email address>
+     send_email=toelapiut7@gmail.com
 
      email=Message(subject,sender=sender_email,recipients=[to])
      email.body=render_template(template+'.txt',**kwargs)
