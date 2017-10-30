f833ebdb128b227153baf91053b84b02230c7fef
toelapiut <toelapiut7@gmail.com>
2017-10-30 21:15:41 +0300

    fixed bugs

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
 
-    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SECRET_KEY = os.environ.get('qwerty12')
     UPLOADED_PHOTOS_DEST='app/static/photos'
     #  email configurations
     MAIL_SERVER = 'smtp.googlemail.com'
@@ -10,14 +10,18 @@ class Config:
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    SUBJECT_PREFIX = 'pitchnation'
+    MAIL_USERNAME = os.environ.get("toelapiut7@gmail.com")
+    MAIL_PASSWORD = os.environ.get("Kinyanjui123")
 
 class ProdConfig(Config):
-    pass
 
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    
 
 class DevConfig(Config):
-    
-     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://toel:KingChase@localhost/pitchnation'
+
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://toel:KingChase@localhost/pitchnation'
     DEBUG = True    
 
 config_options = {
