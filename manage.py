b0859287ae7d7ad8ee20ef1dcdd56a2ddaaf3213
toelapiut <toelapiut7@gmail.com>
2017-10-31 09:56:16 +0300

    fx bug

--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 from app import create_app
 from flask_script import Manager,Server
 from app import create_app,db
-from app.models import User,Role
+from app.models import User,Result
 from flask_migrate import Migrate,MigrateCommand
 
 # Creating app instance
@@ -23,7 +23,7 @@ manager.add_command('db',MigrateCommand)
 
 @manager.shell
 def make_shell_context():
-    return dict(app=app,db=db,User=User,Role = Role)
+    return dict(app=app,db=db,User=User,Result = Result)
 
 
 if __name__=='__main__':
