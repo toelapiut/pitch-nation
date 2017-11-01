f776187903448f241a0a0eb42cc465f1be8767ab
toelapiut <toelapiut7@gmail.com>
2017-11-01 10:57:03 +0300

    removed unnessesary file

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,8 +1,16 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,PasswordField,SubmitField,TextAreaField
+from wtforms import StringField,PasswordField,SubmitField,TextAreaField,SelectField
 from wtforms.validators import Required,Email,EqualTo
 from ..models import User
 
 class UpdateProfile(FlaskForm):
      bio=TextAreaField('Tell us about you',validators=[Required()])
      submit=SubmitField('submit')
+
+    
+
+class pitchIdea(FlaskForm):
+    category = [('business', 'business'),('science', 'science'),('tech', 'tech'),('interview', 'interview')]
+    title=StringField('Pitch Title',validators=[Required()])
+    pitch=TextAreaField('Tell us about your Idea',validators=[Required()])
+    submit=SubmitField('submit')
