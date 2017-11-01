2bb7dfe6793e3d6d035ba6a0038aa98bb525e2a4
toelapiut <toelapiut7@gmail.com>
2017-11-01 10:56:39 +0300

    imported forms

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -4,4 +4,4 @@ from flask import Blueprint
 #the name of the blueprint "Main" and  __name__ variable to find the location of the blueprint.
 
 main=Blueprint('main',__name__)
-from . import views,error
+from . import views,error,forms
