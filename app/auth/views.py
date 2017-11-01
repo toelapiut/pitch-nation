62836eb422908ef65b0687ef85851e9b297b8336
toelapiut <toelapiut7@gmail.com>
2017-11-01 10:55:46 +0300

    fixed bug

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,4 +1,4 @@
-from flask import render_template,redirect,url_for
+from flask import render_template,redirect,url_for,flash,request
 from ..models import User
 from .forms import RegistrationForm,LoginForm
 from .. import db
@@ -10,7 +10,7 @@ from ..email import mail_message
 def login():
      login_form=LoginForm()
      if login_form.validate_on_submit():
-          user=User.query.filter_by(email=Login_form.email.data).first()
+          user=User.query.filter_by(email=login_form.email.data).first()
           if user is not None and user.verify_password(login_form.password.data):
                login_user(user,login_form.remember.data)
                return redirect(request.args.get('next') or url_for('main.index'))
@@ -29,10 +29,11 @@ def register():
           db.session.add(user)
           db.session.commit()
 
-          mail_message('welcome to ..........." email/welcome_user',user.email,user=user)
           return redirect(url_for('auth.login'))
           title="New account"
      return render_template('auth/register.html',registration_form = form)
+
+
 
 @auth.route('/logout')
 @login_required
